30e014d6b6cc61a9a1857377cc79749d423e6ad1
SilviMarku <marku.silvia777@gmail.com>
2023-12-13 13:08:47 +0100

    Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module to initiate a flask app
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """
+    Index route
+    """
+
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
